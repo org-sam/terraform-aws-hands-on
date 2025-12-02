bcfeca2e40ed4832d172c59966b8df4963893674
Samuel Mota <samuel.mota@cogna.com.br>
2025-12-02 16:24:44 -0300

    csi

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -78,31 +78,3 @@ module "eks" {
     var.tags
   )
 }
-
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = var.addon_versions.aws_ebs_csi_driver
-  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [
-    module.eks,
-    aws_eks_pod_identity_association.ebs_csi_driver
-  ]
-}
-
-resource "aws_eks_addon" "efs_csi_driver" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-efs-csi-driver"
-  addon_version            = var.addon_versions.aws_efs_csi_driver
-  service_account_role_arn = aws_iam_role.efs_csi_driver.arn
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [
-    module.eks,
-    aws_eks_pod_identity_association.efs_csi_driver
-  ]
-}
